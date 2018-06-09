8f544a68ad2ab46c7f3b0299475b0b1524282e2e
FedGorunov <pinta.f14@gmail.com>
2018-06-09 19:13:02 +0700

    проверка ключа

--- temp_on_disp.js
+++ temp_on_disp.js
@@ -1,49 +1,51 @@
-const axios = require('axios');
+const axios = require("axios");
 const axiosConfig = {
-    headers:{'Authorization': config.keyEsp}
-}
+  headers: { Authorization: config.keyEsp }
+};
 const config = require("./config");
-const bodyReadTemp ={"pin":"2"};
-const urlReadTemp ="http://"+ config.ipAdress+"/api/devices/dht11/read";
-const urlShowDisp ="http://"+ config.ipAdress+"/api/devices/pcf8574/hd44780/write";
-
+const bodyReadTemp = { pin: "2" };
+const urlReadTemp = "http://" + config.ipAdress + "/api/devices/dht11/read";
+const urlShowDisp =
+  "http://" + config.ipAdress + "/api/devices/pcf8574/hd44780/write";
 
 var measurTemperature = setInterval(measureTemperature(), 3000);
 
-setTimeout(function(){
-    clearInterval(measurTemperature);
-    console.log("The end.");
+setTimeout(function() {
+  clearInterval(measurTemperature);
+  console.log("The end.");
 }, 15000);
 
-
-
 function measureTemperature() {
-    return function () {
-        var promisTemp = axios.post(urlReadTemp, bodyReadTemp, axiosConfig);
-        promisTemp.then(function (response) {
-        
-            showOnDispley(response.data);
-        });
-        promisTemp.catch(function (error) {
-            console.log('error temp: ', error);
-        });
-    };
+  return function() {
+    var promisTemp = axios.post(urlReadTemp, bodyReadTemp, axiosConfig);
+    promisTemp.then(function(response) {
+      showOnDispley(response.data);
+    });
+    promisTemp.catch(function(error) {
+      console.log("error temp: ", error);
+    });
+  };
 }
 
 function showOnDispley(tempJson) {
-    let tempString = "Temperature= " + tempJson["temperature"] + "C    Humidity= " + tempJson["humidity"];
-    let bodyShowDisp = {
-    "SDA": "4",
-        "SCL": "5",
-        "address": config.i2cAdressDisp,
-        "text": tempString
-    }; 
-    console.log("dataTemp=", tempJson);
-    axios.post(urlShowDisp, bodyShowDisp, axiosConfig)
-    .then(function (response) {
-        console.log("Displey: ", response.status);
-    }).catch(function (error) {
-        console.log('error display: ', error.data);
+  let tempString =
+    "Temperature= " +
+    tempJson["temperature"] +
+    "C    Humidity= " +
+    tempJson["humidity"];
+  let bodyShowDisp = {
+    SDA: "4",
+    SCL: "5",
+    address: config.i2cAdressDisp,
+    text: tempString
+  };
+  console.log("dataTemp=", tempJson);
+  axios
+    .post(urlShowDisp, bodyShowDisp, axiosConfig)
+    .then(function(response) {
+      console.log("Displey: ", response.status);
+    })
+    .catch(function(error) {
+      console.log("error display: ", error.data);
     });
 }
- 
